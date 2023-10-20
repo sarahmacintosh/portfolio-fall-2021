e352ca3a202035e220ec354de8c953ac23ef2094
Sarah Macintosh <s5macint@uwaterloo.ca>
2023-10-20 11:30:36 -0400

    updated CV and About

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,10 @@
 		<img src="../assets/glacier-hike.jpeg" alt="Me!">
 			<div>
 				<h1>Hey, I'm Sarah!</h1>
-				<p class="sub-header" >I'm a third year Systems Design Engineering student at the University of Waterloo. I've worked on a wide range of projects where I've gained experience in user experience design, research, project management and front end development.</p>
+				<p class="sub-header" >I'm a fourth year Systems Design Engineering student at the University of Waterloo. I've worked on a wide range of projects where I've gained experience in user experience design, research, project management and front end development.</p>
 				<p>I'm passionate about using a user-centred, data driven approach to design digital and physical products with flawless user experience and a positive environmental impact. I have diverse experience in UX design, UX research, project management and front end development so I have the background to create designs that are simple for developers to implement while serving users effectively.</p>
 				<p>I'm also an avid athlete. I enjoy running, mountain biking, climbing and competing on the Waterloo Varsity cross country ski team. When I'm not being active I enjoy photography, travelling with friends and taking care of my jungle of house plants.</p>
-				<p>Looking for a 4 month co-op placement starting May 2023. Please feel free to reach out!</p>
+				<p>Seeking UX design and UX research positions. Please feel free to reach out!</p>
 			</div>
 		</div>
 		<Footer/>
